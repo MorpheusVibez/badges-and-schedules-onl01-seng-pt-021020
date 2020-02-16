19d684b9480dcd46439f52ae06c16c9d269c8a0e
Andre Skeene <halodre24@gmail.com>
2020-02-16 23:23:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,13 +3,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  array_names = []
   count = 0 
   
   attendees.each do
-  array_names << attendees 
     "Hello, my name is #{attendees}."
      count += 1
+    attendees
   end
-array_names
 end
