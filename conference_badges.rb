ec8bb9b7b2d16e21201827d43a6531cc2a76d470
Andre Skeene <halodre24@gmail.com>
2020-02-17 00:37:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,5 +17,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts "#{assign_rooms}"
+  batch_badge_creator(attendees).each do |name|
+    puts name
+  end
+  assign_rooms(attendees).each do |name|
+    puts name
+  end
 end
