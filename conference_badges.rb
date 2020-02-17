3dc9a3b00257a79da69e7dff6fcc85d0b8508c37
Andre Skeene <halodre24@gmail.com>
2020-02-17 00:03:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees = " ")
-  if badge_maker = " "
+def batch_badge_creator(attendees = name)
+  if badge_maker = name
     return "Hello, my name is Johnny"
   else
     return badges
